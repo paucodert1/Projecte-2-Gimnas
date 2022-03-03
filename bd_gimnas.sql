07b2dcd3c85b1062216e117eadfd21ce8aa033d5
Bruno <94859708+BrunoTomeArias@users.noreply.github.com>
2022-03-03 02:00:42 +0100

    Update bd_gimnas.sql

--- bd_gimnas.sql
+++ bd_gimnas.sql
@@ -16,6 +16,8 @@ CREATE TABLE `Clients` (
 	`telefon` varchar(9) NOT NULL,
 	`condiciofisica` varchar(255),
     `ccc` varchar(24) NOT NULL,
+    `username` varchar(24) NOT NULL,
+    `passwd` varchar(32) NOT NULL,
 	PRIMARY KEY (`DNI`)
 );
 
@@ -34,6 +36,7 @@ CREATE TABLE `Activitat` (
     `nom` varchar(30) NOT NULL,
     `dia` int(1) NOT NULL,
     `id_sala` int NOT NULL,
+    `imatge` varchar(50) NOT NULL,
 	PRIMARY KEY (`id_act`)
 );
 
@@ -61,7 +64,7 @@ CREATE TABLE `Monitor` (
 
 CREATE TABLE `Realitzacio` (
     `idrealitzacio` int NOT NULL AUTO_INCREMENT,
-	`data` DATE NOT NULL,
+	`data` DATE DEFAULT null,
 	`hora` time(6) NOT NULL,
 	`id_act` int NOT NULL,
 	`id_sala` int NOT NULL,
@@ -72,13 +75,6 @@ CREATE TABLE `Realitzacio` (
 CREATE TABLE `Altes` (
 	`data_alta` DATE NOT NULL,
 	`data_baixa` DATE,
-	`DNI` varchar(9) NOT NULL,
-    PRIMARY KEY (`DNI`)
-);
-
-CREATE TABLE `Usuari_web` (
-	`username` varchar(16) NOT NULL,
-	`passwd` varchar(32) NOT NULL,
 	`DNI` varchar(9) NOT NULL,
     PRIMARY KEY (`DNI`)
 );
@@ -114,8 +110,6 @@ ALTER TABLE `Realitzacio` ADD CONSTRAINT `Realitzacio_fk2` FOREIGN KEY (`DNI`) R
 
 ALTER TABLE `Altes` ADD CONSTRAINT `Altes_fk0` FOREIGN KEY (`dni`) REFERENCES `Clients`(`DNI`);
 
-ALTER TABLE `Usuari_web` ADD CONSTRAINT `Usuari_web_fk0` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
-
 ALTER TABLE `Participa` ADD CONSTRAINT `Participa_fk0` FOREIGN KEY (`id_cursa`) REFERENCES `Curses`(`id_cursa`);
 
 ALTER TABLE `Participa` ADD CONSTRAINT `Participa_fk1` FOREIGN KEY (`DNI`) REFERENCES `Clients`(`DNI`);
@@ -147,13 +141,13 @@ END$$
 DELIMITER ;
 
 
-INSERT INTO Clients (DNI, nom, cognom1, cognom2, sexe, comunicaciocomercial, datanaixement, email, telefon, condiciofisica, ccc) VALUES 
-('47137446G', 'Bruno', 'Tomé', 'Arias', 'M', 'SI', '2003-08-09', 'brunota.dam1@alumnescostafreda.cat', '628093249', null, "ES2412491614145851007544"),
-('77383544K', 'Janire', 'Quiles', 'cognom', 'H', 'SI', '2003-08-09', 'brunota.dam1@alumnescostafreda.cat', '662672907', null, "ES6815257321906273010242"),
-('54126466Z', 'Ignasi', 'Cabrera', 'cognom', 'M', 'NO', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '646721702', null, "ES5600368145265920955294"),
-('51833470A', 'Myriam', 'Mari', 'cognom', 'H', 'NO', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '600269857', null, "ES3631901491813119440760"),
-('67289921V', 'Ayoub', 'Rosales', 'cognom', 'M', 'SI', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '625166765', null, "ES0420405698814239157974"),
-('00046319C', 'Sergio', 'Caceres', 'cognom', 'M', 'SI', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '620323809', null, "ES6330726229373463442372");
+INSERT INTO Clients (DNI, nom, cognom1, cognom2, sexe, comunicaciocomercial, datanaixement, email, telefon, condiciofisica, ccc, username, passwd) VALUES 
+('47137446G', 'Bruno', 'Tomé', 'Arias', 'M', 'SI', '2003-08-09', 'brunota.dam1@alumnescostafreda.cat', '628093249', null, "ES2412491614145851007544", 'BrunoTA', MD5('1234')),
+('77383544K', 'Janire', 'Quiles', 'cognom', 'H', 'SI', '2003-08-09', 'brunota.dam1@alumnescostafreda.cat', '662672907', null, "ES6815257321906273010242", 'JanireQ', MD5('1234')),
+('54126466Z', 'Ignasi', 'Cabrera', 'cognom', 'M', 'NO', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '646721702', null, "ES5600368145265920955294", 'IgnasiC', MD5('1234')),
+('51833470A', 'Myriam', 'Mari', 'cognom', 'H', 'NO', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '600269857', null, "ES3631901491813119440760", 'MyriamM', MD5('1234')),
+('67289921V', 'Ayoub', 'Rosales', 'cognom', 'M', 'SI', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '625166765', null, "ES0420405698814239157974", 'AyoubR', MD5('1234')),
+('00046319C', 'Sergio', 'Caceres', 'cognom', 'M', 'SI', '2003-08-09','brunota.dam1@alumnescostafreda.cat', '620323809', null, "ES6330726229373463442372", 'SergioC', MD5('1234'));
 
 INSERT INTO Monitor(DNI_monitor, NSS, telefon, nom, cognom, email) VALUES
 ('08578598N', '408472140491', '623792716', 'Rafael', 'Luz', 'rafaell@gmail.com'),
@@ -168,21 +162,21 @@ INSERT INTO Sala(descripcio, aforament, DNI_monitor) VALUES
 ('Sala de maquines de exercicis Num 2', 40, '17504793E');
 
 
-INSERT INTO Activitat (hora_inici, hora_final, nom, dia, id_sala) VALUES 
-('12:30:00', '13:30:00', 'piscina', 1, 1),
-('14:00:00', '15:00:00', 'fitness', 1, 2),
-('12:30:00', '13:30:00', 'padel', 2, 4),
-('14:00:00', '15:00:00', 'bici', 2, 4),
-('12:30:00', '13:30:00', 'cinta de correr', 3, 2),
-('14:00:00', '15:00:00', 'cycling', 3, 2),
-('12:30:00', '13:30:00', 'body pump', 4, 2),
-('14:00:00', '15:00:00', 'pilates', 4, 3),
-('12:30:00', '13:30:00', 'natació', 5, 1),
-('14:00:00', '15:00:00', 'aquagym', 5, 1),
-('12:30:00', '13:30:00', 'running', 6, 3),
-('14:00:00', '15:00:00', 'zumba', 6, 3),
-('12:30:00', '13:30:00', 'boxa', 7, 2),
-('14:00:00', '15:00:00', 'ioga', 7, 3);
+INSERT INTO Activitat (hora_inici, hora_final, nom, dia, id_sala, imatge) VALUES 
+('12:30:00', '13:30:00', 'piscina', 1, 1, '../img/1.png'),
+('14:00:00', '15:00:00', 'fitness', 1, 2, '../img/2.png'),
+('12:30:00', '13:30:00', 'padel', 1, 4, '../img/3.png'),
+('14:00:00', '15:00:00', 'bici', 2, 4, '../img/4.png'),
+('12:30:00', '13:30:00', 'cinta de correr', 2, 2, '../img/5.png'),
+('14:00:00', '15:00:00', 'cycling', 2, 2, '../img/6.png'),
+('12:30:00', '13:30:00', 'body pump', 3, 2, '../img/7.png'),
+('14:00:00', '15:00:00', 'pilates', 3, 3, '../img/8.png'),
+('12:30:00', '13:30:00', 'natació', 3, 1, '../img/9.png'),
+('14:00:00', '15:00:00', 'aquagym', 4, 1, '../img/10.png'),
+('12:30:00', '13:30:00', 'running', 4, 3, '../img/11.png'),
+('14:00:00', '15:00:00', 'zumba', 4, 3, '../img/12.png'),
+('12:30:00', '13:30:00', 'boxa', 5, 2, '../img/13.png'),
+('14:00:00', '15:00:00', 'ioga', 5, 3, '../img/14.png');
 
 
 INSERT INTO Activitat_lliure(id_act, descripcio) VALUES
@@ -213,15 +207,6 @@ INSERT INTO Altes(data_alta, data_baixa, DNI) VALUES
 ('2022-01-15', null, '00046319C');
 
 
-INSERT INTO Usuari_web (username, passwd, DNI) VALUES 
-('BrunoTA', MD5('1234'), '47137446G'),
-('JanireQ', MD5('1234'), '77383544K'),
-('IgnasiC', MD5('1234'), '54126466Z'),
-('MyriamM', MD5('1234'), '51833470A'),
-('AyoubR', MD5('1234'), '67289921V'),
-('SergioC', MD5('1234'), '00046319C');
-
-
 INSERT INTO Curses (descripcio, distancia, durada) VALUES 
 ('Cursa solidaria a peu al voltant de Tarrega', 12, '4 hores'),
 ('Cursa amb bici per Cervera', 15, '4,5 hores'),
@@ -236,25 +221,9 @@ INSERT INTO Participa (data, hora, id_cursa, DNI) VALUES
 
 INSERT INTO Realitzacio (data, hora, id_act, id_sala, dni) VALUES
 ('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-02-28', '13:30:00', '1', 1, '47137446G'),
-('2022-02-28', '13:30:00', '1', 1, '00046319C'),
-('2022-03-28', '13:30:00', '1', 1, '00046319C');
+(null, '13:30:00', '1', 1, '47137446G'),
+('2022-02-28', '13:30:00', '1', 1, '00046319C');
 
 call calcul_aforament();
+
+SELECT * FROM CLIENTS;
