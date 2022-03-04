b6dfcc1ecfab997a310f238fd2edd59e8db8c5c7
Pau Rubio <94713182+paucodert1@users.noreply.github.com>
2022-03-04 13:25:10 +0100

    Add files via upload
    Perfil de el client i els update acabat

---
+++ PR2 Perfil I modificar acabat/php/login.php
@@ -0,0 +1,24 @@
+<?php 
+include 'consultes.php';
+session_start();
+inicdesessio();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../css/loginanimat.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form class="login" action="" method="POST">
+  <input type="text" placeholder="DNI" name="DNI">
+  <input type="password" placeholder="Contrasenya" name="passwd">
+  <button name="submit">Login</button>
+</form>
+
+</body>
+</html>
